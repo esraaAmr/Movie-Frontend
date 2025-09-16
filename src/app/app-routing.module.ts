16b322bb8c439ae8c1271b033728a462d5c49db9
esraaAmr <esraaamrkandil@gmail.com>
2025-09-16 00:51:57 -0800

    rename the folder

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './features/auth/components/login/login.component';
-import { AdminDashboardComponent } from './features/admin/components/admin/dashboard.component';
-import { UserDashboardComponent } from './features/user/components/user/dashboard.component';
+import { AdminDashboardComponent } from './features/admin/components/dashboard/dashboard.component';
+import { UserDashboardComponent } from './features/user/components/dashboard/dashboard.component';
 import { AuthGuard } from './core/guards/auth.guard';
 import { AdminGuard } from './core/guards/admin.guard';
 import { UserGuard } from './core/guards/user.guard';
